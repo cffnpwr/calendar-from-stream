d5f7b4faf9ffe44d74c3b718e345b8f2bdb8ba8c
Caffeine Power <yuto1001okuda@gmail.com>
2021-12-09 14:12:58 +0900

    [add] google ログイン時にjwtを返すように変更

--- backend/oauth/views.py
+++ backend/oauth/views.py
@@ -1,6 +1,8 @@
+import datetime
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import permissions, status
+import rest_framework_jwt.utils as jwtUtil
 import requests
 
 import cfs.settings
@@ -28,24 +30,31 @@ def googleOAuth2(request):
     if tokenRes.status_code == requests.codes.ok:
         userInfoRes = requests.get('https://www.googleapis.com/oauth2/v2/userinfo',
                                    params={'access_token': tokenResData['access_token']})
-        userId = userInfoRes.json()['id']
-        data = {
-            'id': userId,
-            'accessToken': tokenResData['access_token'],
-            'refreshToken': tokenResData['refresh_token']
-        }
 
-        try:
-            queryset = User.objects.all().get(id=userId)
-            serializer = UserSerializer(instance=queryset, data=data)
+        if userInfoRes.status_code == requests.codes.ok:
+            userId = userInfoRes.json()['id']
+            data = {
+                'id': userId,
+                'accessToken': tokenResData['access_token'],
+                'refreshToken': tokenResData['refresh_token']
+            }
 
-        except:
-            serializer = UserSerializer(data=data)
+            try:
+                queryset = User.objects.get(id=userId)
+                serializer = UserSerializer(instance=queryset, data=data)
 
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
+            except:
+                serializer = UserSerializer(data=data)
 
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            if serializer.is_valid():
+                serializer.save()
+
+                jwtToken = jwtUtil.jwt_encode_handler(
+                    {'userId': userId, 'exp': datetime.datetime.utcnow() + datetime.timedelta(days=7)})
+                return Response({'token': jwtToken}, status=status.HTTP_200_OK)
+
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response(userInfoRes.json(), status=status.HTTP_401_UNAUTHORIZED)
 
     return Response(tokenResData, status=status.HTTP_401_UNAUTHORIZED)
